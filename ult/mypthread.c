1fe137fe7e8603aa615d02bce7694ab89227f657
EthanBerroa <ethan.berroa@gmail.com>
2016-02-01 23:37:01 -0500

    ADDED COMMENTS FOR CLARITY

--- ult/mypthread.c
+++ ult/mypthread.c
@@ -1,4 +1,4 @@
-#include"mypthread.h"
+#include "mypthread.h"
 
 
 static mypthread_real threadPool[THREAD_COUNT];
@@ -67,20 +67,22 @@ int mypthread_create(mypthread_t *thread, const mypthread_attr_t *attr, void *(*
 		fprintf(stderr, "Unable to get context\n");
 		return -1;
 	}
-	ret->ctx.uc_stack.ss_sp = ret->stk;
-	ret->ctx.uc_stack.ss_size = STACK_SIZE;
-	//ret->ctx.ul_link = ;
+	ret->ctx.uc_stack.ss_sp = ret->stk; //setting base address for an alternate stack for the context ret->ctx, which is a char array inside the ret struct itself
+	ret->ctx.uc_stack.ss_size = STACK_SIZE; //setting the stack size, some arbitrary number
+	//ret->ctx.ul_link = ; //ul_link defines the context that will be RESUMED at the end of ret->ctx, i guess this is shorthand for "the current context"
 	
 	struct pthrarg *temp;	
-	temp = (struct pthrarg *) arg;
+	temp = (struct pthrarg *) arg; //why does he do this? he's storing the argument that's going to be passed into the function that the created thread starts with?
 
-	makecontext(&(ret->ctx), (void (*)(void))start_routine, 1, arg);
+	makecontext(&(ret->ctx), (void (*)(void))start_routine, 1, arg); //i believe this sets the created thread in motion
 
-	ret->status = PAUSED;
+	ret->status = PAUSED; //why does he pause here?? presumably this is after the created thread has yielded or something, i'm not sure
 
-	*thread = ret; //This line is important, I think it needs to be fixed
+	*thread = ret; 
 
 	return 0;
+
+	//There is also no reference to currThread in this function, which is curious. One would think that currThread would be set to ret
 }
 
 /*
@@ -102,7 +104,7 @@ void mypthread_exit(void *retval) {
 
 	if(!currThread) {
 		//User calls exit before join or yield, but after create
-		printf("Need to make this edge case\n");
+		printf("Need to make this edge case\n"); //hasn't been handled yet, we might want to change this later
 	}
 
 	currThread->status = UNUSED;
@@ -113,15 +115,15 @@ void mypthread_exit(void *retval) {
 	}
 
 	if(currThread->parent) {
-		setcontext(&(currThread->parent->ctx));
+		setcontext(&(currThread->parent->ctx)); //return to the context of the threads parent after the thread exits, if it had a parent, traversing up a linked list kinda
 	}
 
-	currThread = getPausedThread();
+	currThread = getPausedThread(); //just grabbing any paused thread off the "stack" i guess
 	if(!currThread) {
 		printf("Unable to find paused thread!");
 	}
 
-	currThread->status = ACTIVE;
+	currThread->status = ACTIVE; //resume whatever this thread was doing before it was paused
 
 	if(setcontext(&(currThread->ctx)) == -1) {
 		printf("Unable to set context!");
@@ -130,7 +132,7 @@ void mypthread_exit(void *retval) {
 
 int mypthread_yield(void) {
 	
-	mypthread_t thread =getPausedThread();
+	mypthread_t thread = getPausedThread(); //I assume this gets the thread that the current thread is going to yield TO
 	if(thread == 0) {
 		printf("Error, either by user or yield getPausedThread() failed");
 	}
@@ -138,21 +140,21 @@ int mypthread_yield(void) {
 	if(currThread == NULL) {
 		//Should only happen on very first attempted join/ yield
 		//Means main thread needs to be set up
-		mypthread_t mainThread = getUnusedThread();
+		mypthread_t mainThread = getUnusedThread(); //this grabs the current calling thread? at least i think
 		mainThread->status = PAUSED;
 
 		thread->status = ACTIVE;
 		currThread = thread;
 
-		if(swapcontext(&(mainThread->ctx), &(currThread->ctx)) == -1 ) {
+		if(swapcontext(&(mainThread->ctx), &(currThread->ctx)) == -1 ) { //begin with whatever the thread yielded to wanted to do
 			printf("SwapContext failed\n");
 			return -1;
 		}
-
+		
 		return 0;
 	}
 
-
+	//same stuff as above
 	mypthread_real *oldThread = currThread;
 
 	oldThread->status = PAUSED;
@@ -169,6 +171,8 @@ int mypthread_yield(void) {
 	return 0;
 	
 }
+
+//THIS IS PROBABLY WHERE THE WAITING TIME IS COMING FROM? NOT 100% SURE YET
 
 int mypthread_join(mypthread_t thread, void **retval) {
 
@@ -187,18 +191,19 @@ int mypthread_join(mypthread_t thread, void **retval) {
 		//Should only happen on very first attempted join/ yield
 		//Means main thread needs to be set up
 		mypthread_real *mainThread = getUnusedThread();
-		mainThread->status = WAITING;
-		thread->parent = mainThread;
-		thread->status = ACTIVE;
+		mainThread->status = WAITING; //THIS IS THE CALLING THREAD, THE ONE THAT CALLED THE FUNCTION AND WAITS TO WAIT
+		thread->parent = mainThread;  
+		thread->status = ACTIVE;	//AND THIS IS THE THREAD THAT IS GOING TO BE WAITED FOR
 		currThread = thread;
 
-		if(swapcontext(&(mainThread->ctx), &(currThread->ctx)) == -1 ) {
+		if(swapcontext(&(mainThread->ctx), &(currThread->ctx)) == -1 ) { //do stuff in the thread we're waiting to join with later
 			fprintf(stderr, "SwapContext failed\n");
 			return -1;
 		}
 		return 0;
 	}
 
+	//same as above, set calling thread to wait, and swap contexts with the given thread
 	mypthread_real *oldThread = currThread;
 
 	oldThread->status = WAITING;
@@ -216,4 +221,5 @@ int mypthread_join(mypthread_t thread, void **retval) {
 	return 0;
 	
 }
+
 
